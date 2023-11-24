7d59dbcdd5a8fc05e5220a08b47fbd4dc94507f9
salvador mosqueda <101297047+SalvadorMosqueda@users.noreply.github.com>
2023-11-23 16:42:08 -0800

    update

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,5 @@
 import { User } from "../models/index.js";
-import  {getFilePath} from "../utilis/index.js"
+import { getFilePath } from "../utilis/index.js"
 const getMe = async (req, res) => {
 
     try {
@@ -24,7 +24,7 @@ const getUsers = async (req, res) => {
     const { user } = req
     console.log("user", user)
     try {
-        const users = await User.find({ _id: { $ne: user} }).select("-password")
+        const users = await User.find({ _id: { $ne: user } }).select("-password")
         if (!users) return res.status(404).send({ msg: "No hay usuarios" })
         res.status(200).send({ users })
     } catch (error) {
@@ -36,32 +36,46 @@ const getUsers = async (req, res) => {
 }
 
 const getUser = async (req, res) => {
-    try{
+    try {
         const response = await User.findById(req.params.id).select("-password")
         if (!response) return res.status(404).send({ msg: "Usuario no encontrado" })
         res.status(200).send({ user: response })
 
-    }catch(error){
+    } catch (error) {
         return res.status(500).send({
             error: 'Error en servidor',
             message: error.message
         });
     }
-   
+
 }
 
-const updateUser =async(req,res)=>{
-    const {user}=req
-    const userData =req.body
+const updateUser = async (req, res) => {
+    const { user } = req
+    const userData = req.body
 
-    if(req.files.avatar){
+    if (req.files.avatar) {
         const imagePath = req.files.avatar
-      let response = getFilePath(imagePath)
-        console.log(response)
+        
+        let  img = getFilePath(imagePath)
+
+        userData.avatar = img
+
+        console.log(userData)
     }
 
-  
-    res.status(200).send({msg:"updateUser",user,userData})
+    try{
+        const response = await User.findByIdAndUpdate({_id:user}, userData)
+        if(!response) return res.status(404).send({msg:"Usuario no encontrado"})
+        res.status(200).send({msg:"Usuario actualizado",response})
+
+    } catch (error) {
+        return res.status(500).send({
+            error: 'Error en servidor',
+            message: error.message
+        });
+    }
+    res.status(200).send({ msg: "updateUser",  userData })
 }
 export const UserController = {
     getMe,
